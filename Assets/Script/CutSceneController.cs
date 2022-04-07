711b82476062faf9d66d029dcd2bd1d5fc6c5f38
ValerioRedona <reddyvale1804@gmail.com>
2022-04-07 11:05:37 +0200

    Particelle

---
+++ Assets/Script/CutSceneController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    [SerializeField] GameObject[] cutscenes;
+
+
+    void Start()
+    {
+        cutscenes[0].SetActive(true);
+        cutscenes[1].SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
