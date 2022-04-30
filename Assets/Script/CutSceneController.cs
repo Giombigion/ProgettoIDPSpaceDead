5fb3eac15b53daf7969b3fc4958eec05fdaf6e17
Giacomo Brugnoli <giacomobrugno@gmail.com>
2022-04-30 10:54:12 +0200

    Update Cutscene Skip

--- Assets/Script/CutSceneController.cs
+++ Assets/Script/CutSceneController.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class CutSceneController : MonoBehaviour
 {
+    [SerializeField] public GameObject[] cutscenes;
 
+    public KeyCode key;
+
+    [SerializeField] private PlayableDirector introCutscene = null;
+    [SerializeField] public double skiptime = 50f;
+
+    public static CutSceneController controller;
     // Start is called before the first frame update
-    [SerializeField] GameObject[] cutscenes;
-
-
     void Start()
     {
+        controller = this;
         cutscenes[0].SetActive(true);
     }
 
@@ -20,6 +26,11 @@ public class CutSceneController : MonoBehaviour
         if (!cutscenes[0].activeInHierarchy)
         {
             GameController.instance.panels[2].SetActive(true);
+        }
+
+        if (Input.GetKeyDown(key))
+        {
+            introCutscene.time = skiptime;
         }
     }
 }
