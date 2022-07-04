dbd0df43990346a392b1c809830fa2136921ec87
ValerioRedona <reddyvale1804@gmail.com>
2022-07-04 11:20:09 +0200

    fix menu-cutscene

--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -7,11 +7,19 @@ public class MenuController : MonoBehaviour
 {
     public GameObject CameraCutscene;
     [SerializeField] GameObject menu;
+    public static MenuController menuController;
+    public bool skippable = false;
+
+    private void Start()
+    {
+        menuController = this;
+    }
 
     public void _StartButton()
     {
         CameraCutscene.GetComponent<PlayableDirector>().Play();
         CutSceneController.controller.skipLine.SetActive(true);
+        skippable = true;
         menu.SetActive(false);
     }
 
