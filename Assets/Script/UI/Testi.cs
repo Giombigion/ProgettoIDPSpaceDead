ad8d3b17c939aae966fb2d66f163edb1c0a3deb4
ValerioRedona <reddyvale1804@gmail.com>
2022-07-05 00:38:41 +0200

    Aggiunta Dialoghi #1

--- Assets/Script/UI/Testi.cs
+++ Assets/Script/UI/Testi.cs
@@ -17,10 +17,20 @@ public class TestiHUD
     public string titolo;
 }
 
+[System.Serializable]
+public class TestiDialoghi
+{ 
+    public string narratore;
+
+    [TextArea(5, 10)]
+    public string dialogo;
+}
+
 public class Testi : MonoBehaviour
 {
 
     public List<TestiData> data = new List<TestiData>();
     public List<TestiHUD> testiHUD = new List<TestiHUD>();
+    public List<TestiDialoghi> testiDialoghi = new List<TestiDialoghi>();
 
 }
