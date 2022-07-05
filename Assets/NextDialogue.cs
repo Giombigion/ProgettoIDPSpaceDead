b8bcaa8a947d7343e087347fbdadf819467d2a8f
ValerioRedona <reddyvale1804@gmail.com>
2022-07-05 23:56:52 +0200

    Aggiunta Dialoghi#4 - scorrimento dialoghi

--- Assets/NextDialogue.cs
+++ Assets/NextDialogue.cs
@@ -6,6 +6,7 @@ public class NextDialogue : MonoBehaviour
 {
     float timer = 0f;
     public float waitTime;
+    int seqNum = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +21,16 @@ public class NextDialogue : MonoBehaviour
         
         if(timer > waitTime)
         {
-            gameObject.SetActive(false);
+            if (seqNum <= 12)
+            {
+                GameController.instance.PannelMessage(5, seqNum, true, 2);
+                seqNum++;
+                timer = 0;
+            }
+            else
+            {
+                GameController.instance.PannelMessage(5, 0, false, 2);
+            }
         }
     }
 }
